86594e249939f91a4b4191c2046a2e6473d91546
Sergei <nstandfest@kmail.org>
2022-06-17 12:54:14 -0500

    Updates

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-fetch("/text.json")
+
+document.getElementById('refresh').addEventListener("click", update)
+function update(){
+  fetch("/text.json")
   .then(function (response) {
     return response.json();
   })
   .then(function (myJson) {
-	text = myJson.text.replace("\n", "<br>");
+  text = myJson.text.replace("\n", "<br>");
     document.getElementById("content-text").innerHTML = text;
   })
   .catch(function (error) {
     console.log("Error: " + error);
   });
+}
